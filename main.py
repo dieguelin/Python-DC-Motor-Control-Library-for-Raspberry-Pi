9b4a266b595029e4a07fb96adadcc2c36eb6ea14
dieguelin <correadiego09@gmail.com>
2025-05-28 15:00:47 -0500

    added appropriate output pins for motor

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import sleep
 M1 = MotorInstance() #instanciate motor 1
 print(M1)
 
-M2 = MotorInstance(PIN_PWM = 27,PIN_OUT1 = 1,PIN_OUT2 = 0,PIN_IN1 = 19,PIN_IN2 = 16) #instanciate motor 2
+M2 = MotorInstance(PIN_PWM = 27,PIN_OUT1 = 5,PIN_OUT2 = 6,PIN_IN1 = 19,PIN_IN2 = 16) #instanciate motor 2
 print(M2)
 
 M1.move(720)
